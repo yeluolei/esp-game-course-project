a9129730e06ccc8bb4d90f1f724c22f6eaad9943
sjtu.hupan@gmail.com <sjtu.hupan@gmail.com@f9debc3c-34a5-e391-db32-d2cc2a8413ef>
2011-06-11 13:15:18 +0000

    match.php用于判断匹配label

--- ESPGame/index.php
+++ ESPGame/index.php
@@ -5,8 +5,30 @@
 		<link href="css/index.css" type="text/css" rel="stylesheet"/>
 		<script type="text/javascript" src="js/jquery.js"></script>
 		<script type="text/javascript" src="js/image.js"></script>
+		<script>
+			$(
+					function(){
+						$('#label').bind("click",test);
+						}
+			);
+			function test(){
+				var item = $('#labelbox').attr("value");
+				var id = 1;
+				$.get('match.php',{label:item, picid:id},function(data){
+					if (data.matched == "false"){
+						alert("not matched");
+						}
+					else{
+						alert("matched");
+					}
+				},"json");
+			};
+		</script>
 	</head>
 	<body>
+	<?php
+		session_start();
+	?>
 	<button id="begingame" >开始游戏</button>
 		<div id ="content">
 			<?php include 'header.php';?>
@@ -44,18 +66,18 @@
 			</div>
 			<div id="footer">
 		 	<?php
-				/*$url = 'http://api.flickr.com/services/rest/'; // 请求的URL地址
-				$params = '?method=flickr.photos.search' . // method指明Flickr API所提供的某个方法
-	          			'&api_key=4a06195ccc70207ef0c1710ae6a4ae91' .  // Flickr分配的key
-	          			'&text=sea'; // 关键字
-	//生成的URL
+		 		
+				/*$url = 'http://api.flickr.com/services/rest/'; // 璇锋眰鐨刄RL鍦板潃
+				$params = '?method=flickr.photos.search' . // method鎸囨槑Flickr API鎵�彁渚涚殑鏌愪釜鏂规硶
+	          			'&api_key=4a06195ccc70207ef0c1710ae6a4ae91' .  // Flickr鍒嗛厤鐨刱ey
+	          			'&text=sea'; // 鍏抽敭瀛�	//鐢熸垚鐨刄RL
 	//http://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=2f59b5e190101271213d4b636e30824f&text=sea
-	//如果把改URL黏贴到浏览器的地址栏里，同样可以得到XML文件
+	//濡傛灉鎶婃敼URL榛忚创鍒版祻瑙堝櫒鐨勫湴鍧�爮閲岋紝鍚屾牱鍙互寰楀埌XML鏂囦欢
 				$contents = file_get_contents($url . $params);
-				$xml = new SimpleXMLElement($contents); // 解析XML文件
+				$xml = new SimpleXMLElement($contents); // 瑙ｆ瀽XML鏂囦欢
 				foreach ($xml->photos->photo as $value) {
 	   			$src = 'http://farm' . $value['farm'] . ".static.flickr.com/" .
-	    		$value['server'] . '/' . $value['id'] . '_' . $value['secret'] . '_s.jpg'; // _s用来控制显示图片的大小 reference:http://www.flickr.com/services/api/misc.urls.html
+	    		$value['server'] . '/' . $value['id'] . '_' . $value['secret'] . '_s.jpg'; // _s鐢ㄦ潵鎺у埗鏄剧ず鍥剧墖鐨勫ぇ灏�reference:http://www.flickr.com/services/api/misc.urls.html
 	    		echo "<img src=\"$src\" />";
 				}*/
 			?>
---
+++ ESPGame/match.php
@@ -0,0 +1,40 @@
+<?php
+	include_once 'common/common.php';
+	session_start();
+	$succ = FALSE;
+	$labelid = $_GET['label'];
+	$userid = $_SESSION[userid];
+	$picid = $_GET['picid'];
+	
+	
+	$conn = mysql_connect($cfg_dbhost,$cfg_dbuser,$cfg_dbpwd);
+	if (!$conn)
+	{
+		die("Connecttion to data failed:".mysql_error());
+	}
+	mysql_select_db('esp');
+	
+	$addlabel = "insert into labelforgame values('$labelid','$userid','$picid');";
+	$executeadd = mysql_query($addlabel);
+	$partner = "SELECT * from player where userid = (select partid from player where userid = '$userid');";
+	$executep = mysql_query($partner);
+	if($executep){
+		while($row = mysql_fetch_object($executep)){
+			if($row -> status == 2){
+				$getlabels = "SELECT * from labelforgame where player = '$row->userid'
+				 and labelid = '$labelid' and picid = '$picid';";
+				$executelabels = mysql_query($getlabels);
+				if($executelabels){
+					$succ = TRUE;
+				}
+			}
+		}
+	}
+	
+	if ($succ) {
+		echo json_encode(array('matched'=>'true'));
+	}
+	else{
+		echo json_encode(array('matched'=>'false'));
+	}
+?>
